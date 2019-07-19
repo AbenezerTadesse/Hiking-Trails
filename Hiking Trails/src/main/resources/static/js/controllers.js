499f9a273b5d4fefdb4da0a3fab9b99da124f4d8
Abex47 <abenezerdjn@gmail.com>
2019-07-19 15:17:50 -0600

    added comments.

--- Hiking Trails/src/main/resources/static/js/controllers.js
+++ Hiking Trails/src/main/resources/static/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('app.controllers', []).controller('TrailListController', function($scope, $state, popupService, $window, Trail) {
-  $scope.trails = Trail.query(); //fetch all shipwrecks. Issues a GET to /api/vi/shipwrecks
+  $scope.trails = Trail.query(); //fetch all trails. Issues a GET to /api/vi/trails
 
-  $scope.deleteTrail = function(trail) { // Delete a Shipwreck. Issues a DELETE to /api/v1/shipwrecks/:id
+  $scope.deleteTrail = function(trail) { // Delete a Trail. Issues a DELETE to /api/v1/trails/:id
     if (popupService.showPopup('Really delete this?')) {
       trail.$delete(function() {
         $scope.trails = Trail.query();
@@ -10,25 +10,25 @@ angular.module('app.controllers', []).controller('TrailListController', function
     }
   };
 }).controller('TrailViewController', function($scope, $stateParams, Trail) {
-  $scope.trail = Trail.get({ id: $stateParams.id }); //Get a single shipwreck.Issues a GET to /api/v1/shipwrecks/:id
+  $scope.trail = Trail.get({ id: $stateParams.id }); //Get a single trail.Issues a GET to /api/v1/trails/:id
 }).controller('TrailCreateController', function($scope, $state, $stateParams, Trail) {
-  $scope.trail = new Trail();  //create new shipwreck instance. Properties will be set via ng-model on UI
+  $scope.trail = new Trail();  //create new trail instance. Properties will be set via ng-model on UI
 
-  $scope.addTrail = function() { //create a new shipwreck. Issues a POST to /api/v1/shipwrecks
+  $scope.addTrail = function() { //create a new trail. Issues a POST to /api/v1/trails
     $scope.trail.$save(function() {
-      $state.go('trails'); // on success go back to the list i.e. shipwrecks state.
+      $state.go('trails'); // on success go back to the list i.e. Trials state.
     });
   };
 }).controller('TrailEditController', function($scope, $state, $stateParams, Trail) {
-  $scope.updateTrail = function() { //Update the edited shipwreck. Issues a PUT to /api/v1/shipwrecks/:id
+  $scope.updateTrail = function() { //Update the edited trail. Issues a PUT to /api/v1/trails/:id
     $scope.trail.$update(function() {
-      $state.go('trails'); // on success go back to the list i.e. shipwrecks state.
+      $state.go('trails'); // on success go back to the list i.e. trails state.
     });
   };
 
-  $scope.loadTrail = function() { //Issues a GET request to /api/v1/shipwrecks/:id to get a shipwreck to update
+  $scope.loadTrail = function() { //Issues a GET request to /api/v1/trails/:id to get a Trail to update
     $scope.trail = Trail.get({ id: $stateParams.id });
   };
 
-  $scope.loadTrail(); // Load a shipwreck which can be edited on UI
+  $scope.loadTrail(); // Load a trail which can be edited on UI
 });
